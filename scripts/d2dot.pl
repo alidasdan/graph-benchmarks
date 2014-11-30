4c7a893ba99c20ef64763124b476ae3d2bc49a7b
Ali Dasdan <dasdan@dreamoid.com>
2014-11-29 20:15:46 -0800

    initial commit to github

---
+++ scripts/d2dot.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl -w
+
+# Reads a graph in the dimacs format and converts to the dot format to
+# visualize using graphviz.
+
+# Author: Ali Dasdan
+
+use strict;
+
+if (defined($ARGV[0])) {
+    my $infile = $ARGV[0];
+    unless (open(INFILE, "$infile")) {
+        die("Error: can't open input file $infile.\n");
+    }
+} else {
+    die("Usage: $0 infile [limit]\n");
+}
+
+# option 'limit' to turn off generating dot files for digraphs whose
+# nnodes is larged than limit (because the command 'dot' takes too
+# much time to generate a pdf file).
+my $limit = 500;
+if (defined($ARGV[1])) {
+    $limit = $ARGV[1];
+}
+
+my $nnodes = 0;
+my $nedges = 0;
+
+my $line;
+while (defined($line = <INFILE>)) {
+    chomp($line);
+    my @words = split(/[-\.\s]+/, $line);
+
+    if ($words[0] eq "a") {
+        #                0 1 2 3 4
+        # input format:  a s t w ttime
+        # output format: a s t w ttime
+
+        my $src = $words[1];
+        my $tar = $words[2];
+        my $weight = $words[3];
+        my $ttime = $words[4];
+
+        printf("\t$src -> $tar [label=\"(w=$weight,t=$ttime)\"];\n");
+
+    } elsif ($words[0] eq "p") {
+        #                0 1         2    3 4
+        # input format:  p prob_name-seed n m
+        # output format: p prob_name-seed n m
+
+        if (!defined($words[4])) {
+            die("Error: Probably missing srand see. Expected: p prob_name-seed n m");
+        }
+
+        my $name = $words[1];
+        my $seed = $words[2];
+        $nnodes = $words[3];
+        $nedges = $words[4];
+
+        if ($nnodes > $limit) {
+            printf("Warning: nnodes=$nnodes larger than limit=$limit.\n");
+            $nnodes = 0;
+            last;
+        }
+        
+        printf("digraph $name\_$seed {\n");
+        printf("label=\"(name=$name\_$seed,n=$nnodes,m=$nedges)\";\n");
+
+    } elsif ($words[0] eq "c") {
+
+        next;
+
+    }
+}
+
+close(INFILE);
+
+# print nodes
+if ($nnodes > 0) {
+    for (my $i = 0; $i < $nnodes; $i++) {
+        my $node = $i + 1;
+        printf("\t$node [label=\"$node\"];\n");
+    }
+    printf("}\n");
+}
+
+# end of file
